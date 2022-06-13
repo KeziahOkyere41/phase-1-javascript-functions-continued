0b03f41995318262f9482c819221db407f4191cb
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-06-13 10:23:30 +0000

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function saturdayFun(special){
+    if (special === undefined){
+        return ("This Saturday, I want to roller-skate!");
+    }
+    else {
+        return (`This Saturday, I want to ${special}!`);
+    }
+}
+
+function mondayWork(work){
+    if (work === undefined){
+        return ("This Monday, I will go to the office.");
+    }
+    else{
+        return (`This Monday, I will ${work}.`);
+    }
+}
+
+function wrapAdjective(result = '*') {
+let emphasis = 'You are'
+return function(inner = "handsome"){
+ return `You are ${result}${inner}${result}!`
+}
+}
